c2d0643beeff78b88fc7a360e5cd2e5f4acecd93
unknown <kausikkris10@gmail.com>
2019-01-26 21:44:43 -0800

    reorganized structure

--- ytube_transcript.py
+++ ytube_transcript.py
@@ -1,10 +1,18 @@
 import os
+
+# Imports the Selenium libraries
 import time
 from selenium import webdriver  
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options  
 
-def get_audio(url):
+# Imports the Google Cloud client library
+import io
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+def get_ytube_mp3(ytube_url):
     chrome_options = Options()  
     chrome_options.add_argument('--headless')  
     driver = webdriver.Chrome(options=chrome_options)  
@@ -12,7 +20,7 @@ def get_audio(url):
     result = ''
     driver.get("https://ytmp3.cc/")
 
-    driver.find_element_by_id('input').send_keys(url)
+    driver.find_element_by_id('input').send_keys(ytube_url)
     driver.find_element_by_id('submit').click()
 
     try:
@@ -23,6 +31,32 @@ def get_audio(url):
     
     driver.quit()
     return result
+
+def get_transcript_from_ogg(ogg_name):
+
+    # Instantiates a client
+    client = speech.SpeechClient()
+
+    # The name of the audio file to transcribe
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        ogg_name)
+
+    # Loads the audio into memory
+    with io.open(file_name, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.RecognitionAudio(content=content)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.OGG_OPUS,
+        sample_rate_hertz=48000,
+        language_code='en-US')
+
+    # Detects speech in the audio file
+    response = client.long_running_recognize(config, audio)
+
+    for result in response.results:
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
 
 # Private helper
 def wait_for_dl_link(driver):
