87796e3d87db86c0b89716a77854bed4207725d7
Pedram Amani <34521069+pedramamani@users.noreply.github.com>
2019-01-27 06:16:28 -0800

    Added loading screen

--- extension/background.js
+++ extension/background.js
@@ -9,46 +9,40 @@ chrome.extension.onConnect.addListener(function(newPort) {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
            var videoId = tabs[0].url.match('(?<=watch[?]v=).{11}');
            if (videoId != null) {
-             isCaptionAvailable(videoId[0], percent);
+             //isCaptionAvailable(videoId[0], percent);
              //port.postMessage("noCaption");
              console.log(videoId[0]);
+             chrome.tabs.sendMessage(tabs[0].id, "startLoadingWindow");
            }
          });
       });
  })
 
-function isCaptionAvailable(videoId, percentage) {
-  var oReq = new XMLHttpRequest();
-  oReq.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        if (this.responseText == 'true') {
-          getSummary(videoId, percentage);
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {action: "startLoadingWindow"});
-          });
-        } else {
-          port.postMessage("noCaption");
-        }
-      }
-  };
-
-  oReq.open("GET", "http://127.0.0.1:8000/check?id=" + videoId, true);
-  oReq.send();
-}
-
-function getSummary(videoId, percentage) {
-  var oReq = new XMLHttpRequest();
-  oReq.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-
-      }
-  };
-
-  oReq.open("GET", "http://127.0.0.1:8000/execute?id=" + videoId + "&percentage=" + percentage, true);
-  oReq.send();
-}
-
-// var prevUrl = ""
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// function isCaptionAvailable(videoId, percentage) {
+//   var oReq = new XMLHttpRequest();
+//   oReq.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//         if (true) {
+//           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//             chrome.tabs.sendMessage(tabs[0].id, {action: "startLoadingWindow"});
+//           });
+//         } else {
+//           port.postMessage("noCaption");
+//       }
+//   };
 //
-// });
+//   oReq.open("GET", "http://127.0.0.1:8000/check?id=" + videoId, true);
+//   oReq.send();
+// }
+
+// function getSummary(videoId, percentage) {
+//   var oReq = new XMLHttpRequest();
+//   oReq.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//
+//       }
+//   };
+//
+//   oReq.open("GET", "http://127.0.0.1:8000/execute?id=" + videoId + "&percentage=" + percentage, true);
+//   oReq.send();
+// }
